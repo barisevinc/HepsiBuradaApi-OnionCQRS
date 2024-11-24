9b621dcd9e6905da00296f04e03592af4fd48d4e
Baris Sevinc <barissevinc088@gmail.com>
2024-11-25 02:45:30 +0300

    The model structure of the project was created.

---
+++ Core/HepsiBuradaApi.Domain/Entities/Category.cs
@@ -0,0 +1,33 @@
+﻿using HepsiBuradaApi.Domain.Common;
+using HepsiBuradaApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiBuradaApi.Domain.Entities
+{
+    public class Category:EntityBase
+    {
+        public Category() 
+        {
+
+        }
+
+        public Category(int parentId, string name, int priority)
+        {
+            ParentId = parentId;
+            Name = name;
+            Priority = priority;
+        }
+
+        public required int ParentId { get; set; }
+        public required string Name { get; set; }
+        public required int Priority { get; set; }
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Product> Products { get;
+    }
+}
+
+
