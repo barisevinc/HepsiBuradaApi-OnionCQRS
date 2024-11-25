6a059fb5bfad4f597d3b3f623a1fb5fabfb5a048
Baris Sevinc <barissevinc088@gmail.com>
2024-11-25 23:09:07 +0300

    Added AppDbContext to the Persistence layer and configured SQL Server.

--- Core/HepsiBuradaApi.Domain/Entities/Category.cs
+++ Core/HepsiBuradaApi.Domain/Entities/Category.cs
@@ -26,7 +26,7 @@ namespace HepsiBuradaApi.Domain.Entities
         public required string Name { get; set; }
         public required int Priority { get; set; }
         public ICollection<Detail> Details { get; set; }
-        public ICollection<Product> Products { get;
+        public ICollection<Product> Products { get; set; }
     }
 }
 
---
+++ Infrastructure/HepsiBuradaApi.Persistence/Registration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HepsiBuradaApi.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace HepsiBuradaApi.Persistence
+{
+    public static class Registration
+    {
+        public static void AddPersistance(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(opt => 
+            opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
