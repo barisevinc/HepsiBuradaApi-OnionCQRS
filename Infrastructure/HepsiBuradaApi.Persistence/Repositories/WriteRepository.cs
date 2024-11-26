27eb42dc15b512a2ead84a75e6bf779e6483ac58
Baris Sevinc <barissevinc088@gmail.com>
2024-11-26 15:16:46 +0300

    Created WriteRepository methods.

--- Infrastructure/HepsiBuradaApi.Persistence/Repositories/WriteRepository.cs
+++ Infrastructure/HepsiBuradaApi.Persistence/Repositories/WriteRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using HepsiBuradaApi.Application.Interfaces.Repositories;
+using HepsiBuradaApi.Domain.Common;
+using HepsiBuradaApi.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,40 @@ using System.Threading.Tasks;
 
 namespace HepsiBuradaApi.Persistence.Repositories
 {
-    public class WriteRepository
+    public class WriteRepository<T> : IWriteRepository<T> where T : class, IEntityBase, new()
     {
+        private readonly AppDbContext _appDbContext;
+
+        public WriteRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        private DbSet<T> Table { get=> _appDbContext.Set<T>(); }
+
+        public async Task AddAsync(T entity)
+        {
+            await Table.AddAsync(entity);
+
+        }
+
+        public async Task AddRangeAsync(IList<T> entities)
+        {
+            await Table.AddRangeAsync(entities);
+
+        }
+
+        public async Task HardDeleteAsync(T entity)
+        {
+            await Task.Run(() => Table.Remove(entity));
+
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            await Task.Run(() => Table.Update(entity));
+            return entity;
+
+        }
     }
 }
