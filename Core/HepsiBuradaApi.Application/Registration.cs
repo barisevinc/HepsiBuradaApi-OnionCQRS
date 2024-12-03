780f5969737b56706ee79db321db711da9ccd558
Baris Sevinc <barissevinc088@gmail.com>
2024-12-03 17:54:54 +0300

    Added static method that adds all rules.

--- Core/HepsiBuradaApi.Application/Registration.cs
+++ Core/HepsiBuradaApi.Application/Registration.cs
@@ -1,16 +1,12 @@
 ﻿using FluentValidation;
+using HepsiBuradaApi.Application.Bases;
 using HepsiBuradaApi.Application.Behaviors;
 using HepsiBuradaApi.Application.Exceptions;
+using HepsiBuradaApi.Application.Features.Products.Rules;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Buffers;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HepsiBuradaApi.Application
 {
@@ -22,6 +18,8 @@ namespace HepsiBuradaApi.Application
 
             services.AddTransient<ExceptionMiddleware>();
 
+            services.AddRulesFromAsssemblyContaining(assembly, typeof(BaseRules));
+
             services.AddMediatR(cfg=> cfg.RegisterServicesFromAssembly(assembly));
 
             services.AddValidatorsFromAssembly(assembly);
@@ -29,6 +27,17 @@ namespace HepsiBuradaApi.Application
             ValidatorOptions.Global.LanguageManager.Culture = new CultureInfo("tr");
 
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(FluentValidationBehavior<,>));
+        }
+
+        private static IServiceCollection AddRulesFromAsssemblyContaining(this IServiceCollection services, Assembly assembly, Type type)
+        {
+
+            var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+            foreach (var item in types)
+               services.AddTransient(item);
+                return services;
+            
+
         }
     }
 }
