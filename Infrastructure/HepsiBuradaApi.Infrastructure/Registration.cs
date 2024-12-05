e3ae1bb2347b433da9bc03b94f3cce122fab62b2
Baris Sevinc <barissevinc088@gmail.com>
2024-12-05 21:00:10 +0300

    JWT token registrations were written in the registration class.

--- Infrastructure/HepsiBuradaApi.Infrastructure/Registration.cs
+++ Infrastructure/HepsiBuradaApi.Infrastructure/Registration.cs
@@ -1,6 +1,9 @@
-﻿using HepsiBuradaApi.Infrastructure.Tokens;
+﻿using HepsiBuradaApi.Application.Interfaces.Tokens;
+using HepsiBuradaApi.Infrastructure.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +17,29 @@ namespace HepsiBuradaApi.Infrastructure
     {
         public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            services.Configure<TokenSettings>(configuration.GetSection("JWt"));
+            services.Configure<TokenSettings>(configuration.GetSection("JWT"));
+            services.AddTransient<ITokenService, TokenService>();
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opt =>
+            {
+                opt.SaveToken = true;
+                opt.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"])),
+                    ValidateLifetime = false,
+                    ValidIssuer = configuration["JWT:Secret"],
+                    ValidAudience= configuration["JWT:Audience"],
+                    ClockSkew = TimeSpan.Zero
+
+                };
+            });
         }
     }
 }
