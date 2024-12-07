c87446ec0f60acbabadd38c9393a5c8724f4dd91
Baris Sevinc <barissevinc088@gmail.com>
2024-12-07 22:48:32 +0300

    Added HttpContextAccessor section.

--- Presentation/HepsiBuradaApi.Api/Program.cs
+++ Presentation/HepsiBuradaApi.Api/Program.cs
@@ -12,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
 
 var env = builder.Environment;
