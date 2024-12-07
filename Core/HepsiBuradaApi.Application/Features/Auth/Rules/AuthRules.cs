f09a43ad2b583926aa2f9479f7c68a888a3730fb
Baris Sevinc <barissevinc088@gmail.com>
2024-12-07 22:52:09 +0300

    Using updated.

--- Core/HepsiBuradaApi.Application/Features/Auth/Rules/AuthRules.cs
+++ Core/HepsiBuradaApi.Application/Features/Auth/Rules/AuthRules.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using YoutubeApi.Domain.Entities;
+using HepsiBuradaApi.Domain.Entities;
 
 namespace HepsiBuradaApi.Application.Features.Auth.Rules
 {
