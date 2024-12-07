4a2cda1dedfc0cc1f532342e85e579d9a19b043a
Baris Sevinc <barissevinc088@gmail.com>
2024-12-07 22:36:47 +0300

    While creating the user, if there is a matching user, a class was created that returns an error message.

---
+++ Core/HepsiBuradaApi.Application/Features/Auth/Exception/UserAlreadyExistException.cs
@@ -0,0 +1,14 @@
+﻿using HepsiBuradaApi.Application.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiBuradaApi.Application.Features.Auth.Exception
+{
+    public class UserAlreadyExistException :BaseException
+    {
+        public UserAlreadyExistException() : base("Böyle bir kullanıcı zaten var.") { }
+    }
+}
