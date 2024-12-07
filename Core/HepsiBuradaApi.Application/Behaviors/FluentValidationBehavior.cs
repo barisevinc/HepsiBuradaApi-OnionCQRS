db38051fbf23dea740a412bf27eda484cca14585
Baris Sevinc <barissevinc088@gmail.com>
2024-12-07 22:26:53 +0300

    FluentValidationBehevior updated.

--- Core/HepsiBuradaApi.Application/Behaviors/FluentValidationBehavior.cs
+++ Core/HepsiBuradaApi.Application/Behaviors/FluentValidationBehavior.cs
@@ -1,35 +1,31 @@
-﻿using MediatR;
-using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using MediatR;
 
-namespace HepsiBuradaApi.Application.Behaviors
+namespace HepsiBuradaApi.Application.Beheviors
 {
-    public class FluentValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    public class FluentValidationBehevior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
     {
-        private readonly IEnumerable<IValidator<TRequest>> _validator;
+        private readonly IEnumerable<IValidator<TRequest>> validator;
 
-        public FluentValidationBehavior(IEnumerable<IValidator<TRequest>> validator)
+        public FluentValidationBehevior(IEnumerable<IValidator<TRequest>> validator)
         {
-            _validator = validator; 
+            this.validator = validator;
         }
         public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
-
             var context = new ValidationContext<TRequest>(request);
-            var failtures = _validator
-            .Select(v => v.Validate(context))
-            .SelectMany(result => result.Errors)
-            .GroupBy(x => x.ErrorMessage)
-            .Select(x => x.First()).Where(f => f != null).ToList();
+            var failtures = validator
+                .Select(v => v.Validate(context))
+                .SelectMany(result => result.Errors)
+                .GroupBy(x => x.ErrorMessage)
+                .Select(x => x.First())
+                .Where(f => f != null)
+                .ToList();
 
-            if (failtures.Any()) {
-
+            if (failtures.Any())
                 throw new ValidationException(failtures);
-            }
+
             return next();
         }
     }
