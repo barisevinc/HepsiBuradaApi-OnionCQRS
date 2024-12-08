beb734efec63adadff13a30de78de5cf8dfcd46e
Baris Sevinc <barissevinc088@gmail.com>
2024-12-08 12:57:20 +0300

    An exception class was written for login.

---
+++ Core/HepsiBuradaApi.Application/Features/Auth/Exception/EmailOrPasswordShouldNotBeInvalidException.cs
@@ -0,0 +1,9 @@
+﻿using HepsiBuradaApi.Application.Bases;
+
+namespace HepsiBuradaApi.Application.Features.Auth.Exception
+{
+    public class EmailOrPasswordShouldNotBeInvalidException : BaseException
+    {
+        public EmailOrPasswordShouldNotBeInvalidException() : base("Kullanıcı adı veya şifre hatalı.") { }
+    }
+}
