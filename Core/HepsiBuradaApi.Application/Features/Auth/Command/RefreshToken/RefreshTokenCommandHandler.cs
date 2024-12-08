2a267caed6d2f7439199764dc5d00529ef64d3e8
Baris Sevinc <barissevinc088@gmail.com>
2024-12-08 19:50:03 +0300

    RefreshToken Handler, Validator, Response, Request, Handler operations were performed.

---
+++ Core/HepsiBuradaApi.Application/Features/Auth/Command/RefreshToken/RefreshTokenCommandHandler.cs
@@ -0,0 +1,54 @@
+﻿using HepsiBuradaApi.Application.Bases;
+using HepsiBuradaApi.Application.Features.Auth.Rules;
+using HepsiBuradaApi.Application.Interfaces.AutoMapper;
+using HepsiBuradaApi.Application.Interfaces.Tokens;
+using HepsiBuradaApi.Application.UnitOfWorks;
+using HepsiBuradaApi.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiBuradaApi.Application.Features.Auth.Command.RefreshToken
+{
+    public class RefreshTokenCommandHandler : BaseHandler,IRequestHandler<RefreshTokenCommandRequest, RefreshTokenCommandResponse>
+    {
+        private readonly AuthRules authRules;
+        private readonly ITokenService tokenService;
+        private readonly UserManager<User> userManager;
+        public RefreshTokenCommandHandler(AuthRules authRules,ITokenService tokenService,IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor, UserManager<User> userManager) : base(mapper, unitOfWork, httpContextAccessor)
+        {
+            this.authRules = authRules;
+            this.tokenService = tokenService;
+            this.userManager = userManager;
+        }
+
+        public async Task<RefreshTokenCommandResponse> Handle(RefreshTokenCommandRequest request, CancellationToken cancellationToken)
+        {
+            ClaimsPrincipal? pricipal = tokenService.GetPrincipalFromExpiredToken(request.AccessToken);
+            string email = pricipal.FindFirstValue(ClaimTypes.Email);
+
+            User? user = await userManager.FindByEmailAsync(email);
+            IList<string> roles = await userManager.GetRolesAsync(user);
+
+            await authRules.RefreshTokenShouldNotBeExpired(user.RefreshTokenExpiryTime);
+
+            JwtSecurityToken newAccessToken = await tokenService.CreateToken(user, roles);
+            string newRefreshToken =  tokenService.GenerateRefreshToken();
+
+            user.RefreshToken = newRefreshToken;
+            await userManager.UpdateAsync(user);
+            return new()
+            {
+                AccessToken = new JwtSecurityTokenHandler().WriteToken(newAccessToken),
+                RefreshToken = newRefreshToken
+            };
+        }
+    } 
+}
---
+++ Core/HepsiBuradaApi.Application/Features/Auth/Command/RefreshToken/RefreshTokenCommandRequest.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiBuradaApi.Application.Features.Auth.Command.RefreshToken
+{
+    public class RefreshTokenCommandRequest : IRequest<RefreshTokenCommandResponse>
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
