8a3b5b63d4ee089f668753a570ac42b2fdab8c9d
Baris Sevinc <barissevinc088@gmail.com>
2024-12-09 12:46:25 +0300

    RedisCacheBehavior.cs class created.

---
+++ Core/HepsiBuradaApi.Application/Behaviors/RedisCacheBehavior.cs
@@ -0,0 +1,40 @@
+﻿using HepsiBuradaApi.Application.Interfaces.RedisCache;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiBuradaApi.Application.Behaviors
+{
+    public class RedisCacheBehavior<TRequest, Tresponse> : IPipelineBehavior<TRequest, Tresponse>
+    {
+        private readonly IRedisCacheService redisCacheService;
+
+        public RedisCacheBehavior(IRedisCacheService redisCacheService)
+        {
+            this.redisCacheService = redisCacheService;
+        }
+
+        public async Task<Tresponse> Handle(TRequest request, RequestHandlerDelegate<Tresponse> next, CancellationToken cancellationToken)
+        {
+            if (request is ICacheableQuery query) 
+            {
+                var cacheKey = query.CacheKey;
+                var cacheTime = query.CacheTime;
+
+                var cacheData = await redisCacheService.GetAsync<Tresponse>(cacheKey);
+                if (cacheData is not null)
+                    return cacheData;
+
+                var response = await next();
+                if (response is not null)
+                    await redisCacheService.SetAsync(cacheKey, response, DateTime.Now.AddMinutes(cacheTime));
+
+                return response;
+            }
+            return await next();
+        }
+    }
+}
