0dd9285647264cd0efed13c68cc63f5a38c4e8b2
Baris Sevinc <barissevinc088@gmail.com>
2024-12-09 12:50:02 +0300

    A method that brings all brands was written.

---
+++ Core/HepsiBuradaApi.Application/Features/Brands/Queries/GetAllBrands/GetAllBrandsQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using HepsiBuradaApi.Application.Bases;
+using HepsiBuradaApi.Application.Interfaces.AutoMapper;
+using HepsiBuradaApi.Application.UnitOfWorks;
+using HepsiBuradaApi.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiBuradaApi.Application.Features.Brands.Queries.GetAllBrands
+{
+    public class GetAllBrandsQueryHandler : BaseHandler, IRequestHandler<GetAllBrandsQueryRequest, IList<GetAllBrandsQueryResponse>>
+    {
+        public GetAllBrandsQueryHandler(IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
+        {
+        }
+
+        public async Task<IList<GetAllBrandsQueryResponse>> Handle(GetAllBrandsQueryRequest request, CancellationToken cancellationToken)
+        {
+            var brands = await unitOfWork.GetReadRepository<Brand>().GetAllAsync();
+            return mapper.Map<GetAllBrandsQueryResponse, Brand>(brands);
+        }
+    }
+}
