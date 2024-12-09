5c5b49fc0df09468f2668b0a3852cf3a6068bf8d
Baris Sevinc <barissevinc088@gmail.com>
2024-12-09 12:51:17 +0300

    Cache operation specified.

--- Core/HepsiBuradaApi.Application/Features/Products/Queries/GeAllProducts/GetAllProductsQueryRequest.cs
+++ Core/HepsiBuradaApi.Application/Features/Products/Queries/GeAllProducts/GetAllProductsQueryRequest.cs
@@ -1,9 +1,12 @@
-﻿using MediatR;
+﻿using HepsiBuradaApi.Application.Interfaces.RedisCache;
+using MediatR;
 
 namespace HepsiBuradaApi.Application.Features.Products.Queries.GeAllProducts
 {
-    public class GetAllProductsQueryRequest : IRequest<IList<GetAllProductsQueryResponse>>
+    public class GetAllProductsQueryRequest : IRequest<IList<GetAllProductsQueryResponse>>, ICacheableQuery
     {
+        public string CacheKey => "GetAllProducts";
 
+        public double CacheTime => 60;
     }
 }
